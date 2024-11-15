7a8e033a82c15a9456018334485e83ca15a012e6
Tim Callow <timjcallow@gmail.com>
2024-11-15 10:25:12 +0100

    Add schedule command (essentially a copy of rerun)

--- datalad_slurm/tests/test_register.py
+++ datalad_slurm/tests/test_register.py
@@ -5,12 +5,17 @@ def test_register():
     import datalad.api as da
     assert hasattr(da, 'schedule')
     assert hasattr(da, 'finish')
+    assert hasattr(da, 'reschedule')
     assert_result_count(
         da.schedule(),
         1,
         action='demo')
     assert_result_count(
         da.finish(),
+        1,
+        action='demo')
+    assert_result_count(
+        da.reschedule(),
         1,
         action='demo')
 
